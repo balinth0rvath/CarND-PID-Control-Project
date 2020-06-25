d1ef3bf38e8d933e802f6e9d7ef9356662fbea26
balinth0rvath <horvath0balint@gmail.com>
2020-06-25 09:36:23 +0200

    Add measurements

--- src/main.cpp
+++ src/main.cpp
@@ -35,14 +35,13 @@ int main() {
 
   PID pid;
 
-	std::vector<double> p = {0.5,0.006,3.0};
-	std::vector<double> dp = {0.1,0.001,0.5};
+	int lap_counter = 1;
+	// Good P I D params from the previous measurements
+	std::vector<double> p = {0.4,0.0059,2.7};
+	std::vector<double> dp = {0.04,0.00059,0.27};
 	pid.Init(p[0],p[1],p[2]);
 
-	std::cout << std::endl;
-	std::cout << "First try: " << p[0] << " " <<  p[1] << " " << p[2] << std::endl; 
-	std::cout << "       dp: " << dp[0] << " " << dp[1] << " " << dp[2] << std::endl; 
-	std::cout << std::endl;
+	std::cout << "lap:" << lap_counter << " " << p[0] << " " <<  p[1] << " " << p[2]; 
 
 	double sum_error = 0.0;
 	double best_error = 0.0;
@@ -54,7 +53,14 @@ int main() {
    * TODO: Initialize the pid variable.
    */
 
-  h.onMessage([&pid,&sum_error,&best_error,&counter,&param_idx,&p,&dp, &state](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  h.onMessage([	&pid,	
+								&sum_error,
+								&best_error,
+								&counter,
+								&param_idx,
+								&p,&dp, 
+								&lap_counter,
+								&state](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -83,85 +89,73 @@ int main() {
 
 					pid.UpdateError(cte);
 					double error = pid.TotalError();
+					// cumulated error in one lap
 					sum_error += pow(error,2.0);
 					
 					steer_value = error;
 
+					// limiting steer value prevents oscillating
 					if (steer_value<-0.7) steer_value = -0.7;
 					if (steer_value>0.7) steer_value = 0.7;
 					
          	double throttle = pid.GetThrottle(speed); 
+					// counter contains the number of samples received from the simulator 
+					// 1700 is approx one lap on this config 
 					counter--;
 					if (!counter)
 					{
 						if (!best_error)
 						{
 							best_error = sum_error;
-							std::cout << "best error was set first time to " << best_error << std::endl;	
 						}
 						counter = 1700;
 
-						//std::cout << "------------------------------------" << std::endl;
-						//std::cout << "A lap passed" << std::endl;
-						std::cout << "------------------------------------" << std::endl;
-						std::cout << "Actual error: " << sum_error << std::endl;
-						std::cout << "param index: " << param_idx << std::endl;
+						std::cout << " error: " << sum_error << std::endl;
+						// Twiddle implemented here:
 						if (state == TwiddleState::INC_CHECK)
 						{
 							if (sum_error < best_error)
 							{
 								best_error = sum_error;
 								dp[param_idx] *= 1.1;
-								//std::cout << "Incrementing succeeded, dp*1.1=" << dp[param_idx] << std::endl;
 								state=TwiddleState::INC;
-								// increment param
 								param_idx++;
 								if (param_idx>2) param_idx=0;
 							} else
 							{
-								//std::cout << "Increment failed, p = " << p[param_idx] << std::endl;
 								p[param_idx] -= 2 * dp[param_idx];
-								//std::cout << "p = p - 2 * dp  = " << p[param_idx] << std::endl;
 								state=TwiddleState::DEC_CHECK;	
 							}
 						} else if (state==TwiddleState::DEC_CHECK)
 						{
 							state=TwiddleState::INC;
-							//std::cout << "decrementing fork next stage" << std::endl;
 							// increment param
 							if (sum_error < best_error)
 							{
 								best_error = sum_error;
 								dp[param_idx] *= 1.1;
-								//std::cout << "second incrementing succeeded, dp*1.1=" << dp[param_idx] << std::endl;
 							} else
 							{
 								p[param_idx] += dp[param_idx];
 								dp[param_idx] *= 0.9;
-								//std::cout << "second incrementing failed, dp*0.9=" << dp[param_idx] << std::endl;
 							}
 							param_idx++;
 							if (param_idx>2) param_idx=0;
 						}
 						if (state==TwiddleState::INC)
 						{
-							//std::cout << "increment p=" << p[param_idx] << " with dp=" << dp[param_idx] << std::endl;
 							p[param_idx] += dp[param_idx];							
 							state=TwiddleState::INC_CHECK;
 						}
 	
 						sum_error = 0.0;
-						std::cout << std::endl;
-						std::cout << "Now trying: " << p[0] << " " << p[1] << " " << p[2]  << std::endl; 
-						std::cout << "        dp: " << dp[0] << " " << dp[1] << " " << dp[2] << std::endl; 
+						lap_counter ++;
+						// log p i d values after each lap
+						std::cout << "lap:" << lap_counter << " " << p[0] << " " <<  p[1] << " " << p[2]; 
 						std::cout << std::endl;
 						pid.Init(p[0],p[1],p[2]);
-						//param_idx++;
 
 					}
-          // DEBUG
-          //std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-          //          << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
